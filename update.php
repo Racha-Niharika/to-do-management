3de98913e4bdeaafd6288c2404972958878bbbd3
sumitth456 <140262337+sumitth456@users.noreply.github.com>
2024-02-17 22:58:30 +0530

    Update update.php

--- update.php
+++ update.php
@@ -148,18 +148,8 @@ form > label{
     <div class="wrapper">
         <h2>Status Form</h2>
 
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-    <h3>Task Name</h3>
-    <div id="input1">
-       <select id="taskName" name="taskName" required>
-    <?php
-    // Populate dropdown list with tasks
-    while ($row = mysqli_fetch_assoc($result)) {
-        $selected = isset($_GET['taskName']) && $_GET['taskName'] === $row['name'] ? 'selected' : '';
-        echo "<option value='" . htmlspecialchars($row['name']) . "' $selected>" . htmlspecialchars($row['name']) . "</option>";
-    }
-    ?>
-</select>
+       <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <input type="hidden" name="taskName" value="<?php echo htmlspecialchars($taskName); ?>">
             
             </div>
 
