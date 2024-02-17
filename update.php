a1bbfd4dcf34a199d5bb50d40619ae096786693a
sumitth456 <140262337+sumitth456@users.noreply.github.com>
2024-02-17 22:33:26 +0530

    Update update.php

--- update.php
+++ update.php
@@ -37,58 +37,113 @@ if (!$result) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Status Form</title>
     <link rel="stylesheet" href="status.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Salsa&display=swap" rel="stylesheet">
+    <style>
+        body {
+            background: #4070f4;
+            font-family: "Kanit", sans-serif;
+            }
+
+        h2{
+            color: rgb(95, 34, 156);
+            text-align: center;
+            font-size: 2rem;
+            text-decoration: underline;
+        }
+
+        .wrapper{
+           
+            height: 65vh;
+            width: 35%;
+            /* background:rgb(147, 243, 51); */
+            background: white;
+            border-radius: 15px;
+            border: 0;
+            margin: auto;
+            justify-content: center;
+            align-items: center;
+            /* box-shadow: 0 0 10px 5px rgba(245, 222, 179, 0.8); */
+            box-shadow: 0 0 25px rgba(126, 245, 8, 0.8); 
+            
+        }
+form{
+    /* background: blue; */
+    margin: -30px 4px;
+    padding: 4px 50px;
+}
+
+h3{
+    color: black;
+    font-size: 1.4rem;
+    margin: 20px 4px;
+    
+}
+form > label{
+    font-size: 1.3rem;
+}
+        #input1 input{
+            height: 25px;
+            width: 300px;
+            margin: 0px 3px;
+            padding: 3px 4px;
+            border: 2px solid black;
+            border-radius: 10px;
+        }
+
+     #status{
+            height: 28px;
+            width: 300px;
+            margin: 2px 3px;
+            padding: 3px 54px;
+            font-size: 1rem;
+            border-radius: 10px;
+        }
+
+        #status:hover{
+            cursor: pointer;
+        }
+ 
+        .text{
+           margin: 7px 50px;
+        }
+
+        .text a{
+        color:rgb(28, 25, 25);
+        transition: all ease 0.5s;
+        }
+        .text a:hover{
+            color: red;
+        }
+        #button button {
+        font-weight: bold;
+    }
+        
+        button{
+            margin: 30px 5px;
+            padding: 2px 5px;
+            border-radius: 10px;
+            height: 40px;
+            width: 110px;
+            font-size: 1rem;
+            font-weight: 500;
+            background: lawngreen;
+            color: black;
+        }
+        button:hover{
+            cursor: pointer;
+            background:limegreen;
+            color: white;
+
+        }
+
+        
+    </style>
 </head>
   
-        table {
-            border-collapse: collapse;
-            width: 100%;
-        }
+       
 
-        th, td {
-            text-align: center;
-            padding: 10px;
-        }
-
-        th {
-            background-color: #3B9EBF;
-        }
-
-        tr:nth-child(even) {
-            background-color: #90EE90;
-        }
-        tr:nth-child(odd) {
-            background-color: #cbcccb;
-        }
-
-        .search-container {
-            margin: 40px 0;
-        }
-
-        input[type=text] {
-    width: 200px; /* Adjust the width as needed */
-    padding: 10px; /* Adjust the padding as needed */
-    margin: 8px 0;
-    box-sizing: border-box;
-    border: 2px solid #ccc;
-    border-radius: 4px;
-    background-color: #cbcccb;
-    font-size: 16px;
-}
-
-
-        input[type=text]:focus {
-            outline: none;
-            border-color: #0066cc;
-        }
-        .center {
-  margin-left: auto;
-  margin-right: auto;
-}
-
-.highlight {
-            background-color: yellow; /* You can change this color */
-        }
-</style>
 <body>
     <div class="wrapper">
         <h2>Status Form</h2>
