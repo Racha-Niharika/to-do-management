17cb4c0b97c452a74caf4ec9dd0407c8bb90d605
sumitth456 <140262337+sumitth456@users.noreply.github.com>
2024-02-17 20:39:00 +0530

    Update delete.php

--- delete.php
+++ delete.php
@@ -3,7 +3,7 @@
 include("connect.php");
 
 // SQL query to delete all records from the tform table
-$sql = "DELETE FROM tform";
+$sql = "DELETE FROM user1";
 
 if ($con->query($sql)) {
     echo "All records deleted successfully";
